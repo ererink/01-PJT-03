3c9aaa36fb1c86207acd02b46da36c6ac655f47c
ererink <yelkim0210@gmail.com>
2022-07-29 17:42:17 +0900

    2회차 김예린 제출

--- 2회차/김예린/2_문자열의거울상.py
+++ 2회차/김예린/2_문자열의거울상.py
@@ -1,3 +1,20 @@
 import sys
 
+T = int(input())
+for test_case in range(1, T + 1):
+
+
+    list1 = ['b', 'd', 'p', 'q']
+    list2 = ['d', 'b', 'q', 'p']
+    
+    list1 = list2
+    
+    chars = input()
+    result = []
+    
+    for i in range(len(list1)):
+        result.append(chars[i])
+        
+    print(result[::-1])
+
 sys.stdin = open("_문자열의거울상.txt")
--- 2회차/김예린/5_신용카드만들기2.py
+++ 2회차/김예린/5_신용카드만들기2.py
@@ -1,3 +1,28 @@
 import sys
 
+
+T = int(input())
+for test_case in range(1, T + 1):
+    card_nums = input().split()
+    result=[]
+    for i in card_nums:
+        result += i
+    
+        if result[0] == '3' or '4' or '5' or '6' or '9': 
+            if len(result) == 16:
+                print('#{} {}'.format(test_case, '1'))
+            elif '-' in result:
+                for _ in result:
+                    result.remove('-')
+                    
+                    if len(result) == 16:
+                        break
+                    
+                print('#{} {}'.format(test_case, '1'))
+            
+            else:
+                print('#{} {}'.format(test_case, '0'))
+        else:
+            print('#{} {}'.format(test_case, '0'))
+            
 sys.stdin = open("_신용카드만들기2.txt")
